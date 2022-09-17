8d12127c499a93283e2cadf91e05c8c64886069e
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-17 16:56:08 +0200

    blog detail page made

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import "./Blog.css"
+import { useParams } from 'react-router-dom';
+import useFetch from "../../hooks/useFetch"
 const Blog = () => {
+    const {id}=useParams();
+    const url=`http://localhost:8000/BlogsJsonDB/`+id;
+    const {error,loading,data}=useFetch(url);
     return (
-        <div>
-            Blog
+        <div className='blog'>
+            {error && <p className='error'>{error}</p>}
+            {loading && <p className='loading'>Loading...</p>}
+            {data &&(
+                <>
+                <h2 className='page-title'>{data.title}</h2>
+                <p className='time'>Reading time : {data.readableTime}</p>
+                <ul>
+                    {data.categories.map((item)=>(
+                        <li key={item}>{item}</li>))}
+                </ul>
+                <p className='info'>{data.description}</p>
+                </>
+                )}
         </div>
     );
 }
