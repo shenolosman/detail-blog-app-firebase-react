bee78eb6b5ee47f917bcca0c7d702adea4276e0f
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-19 15:04:04 +0200

    for colorful theme implamented themecontext

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,11 @@
+import React, { createContext } from "react";
+
+export const ThemeContext = createContext();
+const ThemeProvider = ({ children }) => {
+
+  return <ThemeContext.Provider>
+    {children}
+  </ThemeContext.Provider>;
+};
+
+export default ThemeProvider;
